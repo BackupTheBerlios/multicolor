9023ef1f90b5fe270790cd94170b98a517c8a51a
Thomas Giesel <skoe@directbox.com>
2009-03-21 23:57:29 +0100

    Add Mac OS X cross toolchain

--- make/mac-darwin-cross/cross-gcc.mk
+++ make/mac-darwin-cross/cross-gcc.mk
@@ -28,44 +28,36 @@ here := $(abspath $(dir $(lastword $(MAKEFILE_LIST))))
 # after adding some paths to this it will be used as PATH
 path        := $(PATH)
 
-
 build-dir   := $(here)/build
-archive-dir := $(here)/archive
+archive-dir := $(here)/../archive
 
-odcctools   := odcctools-20060413
-gcc         := gcc-4.3.3
+odcctools   := odcctools-9.2-r277
+sdk         := MacOSX10.4u.sdk
+gcc         := gcc-5465
 
 odcctools-components :=  ar as include libstuff libmacho ld man misc 
 
 cross       := i686-apple-darwin9
-gccprefix   := /opt/cross/$(cross)-$(gcc)
-
-sudo        := sudo
+gccprefix   := $(HOME)/cross/$(cross)-$(gcc)
 
 path        := $(gccprefix)/bin:$(path)
 
-.PHONY: odcctools
-odcctools: $(build-dir)/1-odcctools.done
+.PHONY: all
+all: $(build-dir)/3-gcc-c.done
 
 ###############################################################################
 # odcctools
-
-ifeq ("`uname -m | grep 64`", "")
-odcc_flags :=
-else
-odcc_flags := "-m32"
-endif
 
 # build odcctools
 $(build-dir)/1-odcctools.done: $(build-dir)/$(odcctools)
 	mkdir -p $(build-dir)/obj-$(odcctools)
 	cd $(build-dir)/obj-$(odcctools) && \
-		CFLAGS=$(odcc_flags) LDFLAGS=$(odcc_flags) \
+		CFLAGS=-m32 LDFLAGS=-m32 \
 			$(build-dir)/$(odcctools)/configure \
 			--target=$(cross) --prefix=$(gccprefix) \
 			--with-sysroot=$(gccprefix)
 	make -C $(build-dir)/obj-$(odcctools) COMPONENTS="$(odcctools-components)"
-	$(sudo) make -C $(build-dir)/obj-$(odcctools) install \
+	make -C $(build-dir)/obj-$(odcctools) install \
 		COMPONENTS="$(odcctools-components)"
 	touch $@
 
@@ -74,9 +66,55 @@ $(build-dir)/$(odcctools): $(archive-dir)/$(odcctools).tar.bz2
 	mkdir -p $(build-dir)
 	tar xjf $(archive-dir)/$(odcctools).tar.bz2 -C $(build-dir)
 	patch -d $(build-dir)/$(odcctools) -p 1 -i $(here)/patches/odcctools-no-long-double.patch
+	patch -d $(build-dir)/$(odcctools) -p 1 -i $(here)/patches/odcctools-qsort_r.patch
+	patch -d $(build-dir)/$(odcctools) -p 0 -i $(here)/patches/odcctools-as.patch
 	touch $@
 
 # download odcctools
 $(archive-dir)/$(odcctools).tar.bz2:
+	mkdir $(build-dir)/tmp
+	cd $(build-dir)/tmp && svn co http://iphone-dev.googlecode.com/svn/branches/odcctools-9.2-ld/ $(odcctools) -r 277
+	rm -rf $(build-dir)/tmp/$(odcctools)/.svn
+	tar cjf $@ -C $(build-dir)/tmp $(odcctools)
+
+###############################################################################
+# SDK
+
+# unpack odcctools
+$(build-dir)/2-sdk.done: $(archive-dir)/$(sdk).tar.bz2
+	mkdir -p $(build-dir)
+	tar xjf $(archive-dir)/$(sdk).tar.bz2 -C $(build-dir)
+	mkdir -p $(gccprefix)
+	cp -rf $(build-dir)/$(sdk)/* $(gccprefix)
+
+###############################################################################
+# GCC Step 1
+
+# build gcc - C only
+$(build-dir)/3-gcc-c.done: $(build-dir)/$(gcc) \
+		$(build-dir)/1-odcctools.done \
+		$(build-dir)/2-sdk.done
+	mkdir -p $(build-dir)/obj-$(gcc)
+	cd $(build-dir)/obj-$(gcc) && PATH=$(path) CFLAGS=-m32 LDFLAGS=-m32 \
+		$(build-dir)/$(gcc)/configure \
+		--target=$(cross) --prefix=$(gccprefix) \
+		--with-sysroot=$(gccprefix) \
+		--disable-checking --enable-static --enable-shared --disable-nls \
+		--disable-multilib --enable-languages=c,c++ \
+		--with-as=$(gccprefix)/bin/$(cross)-as \
+		--with-ld=$(gccprefix)/bin/$(cross)-ld 
+	PATH=$(path) make -C $(build-dir)/obj-$(gcc)
+	PATH=$(path) make -C $(build-dir)/obj-$(gcc) install
+	touch $@
+
+# unpack gcc
+$(build-dir)/$(gcc): $(archive-dir)/$(gcc).tar.gz
+	mkdir -p $(build-dir)
+	tar xzf $(archive-dir)/$(gcc).tar.gz -C $(build-dir)
+	patch -d $(build-dir)/$(gcc) -p 1 -i $(here)/patches/gcc-4.0.1-collect2-open.patch
+	touch $@
+
+# download gcc
+$(archive-dir)/$(gcc).tar.gz:
 	mkdir -p $(archive-dir)
-	cd $(archive-dir) && wget http://biolpc22.york.ac.uk/pub/linux-mac-cross/$(odcctools).tar.bz2
+	cd $(archive-dir) && wget http://www.opensource.apple.com/darwinsource/tarballs/other/$(gcc).tar.gz
