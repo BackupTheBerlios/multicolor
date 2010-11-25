d05f56728317531a9e4335c5d36e8cedc7ebf79b
Skoe <skoe@directbox.com>
2010-11-25 22:52:34 +0100

    Fixed XPM conversion

--- make/common/rules.mk
+++ make/common/rules.mk
@@ -79,7 +79,10 @@ $(objdir)/%.o: $(srcdir)/%.c $(headers) | $(objdir) check-environment
 #
 $(objdir)/%.xpm: $(srcdir)/../res/%.png | $(objdir) check-environment
 	convert $< $@.tmp.xpm
-	cat $@.tmp.xpm | sed "s/static char/static const char/;s/\.tmp//;s/\.xpm/_xpm/" > $@
+	cat $@.tmp.xpm | sed "\
+			s/static char/static const char/;\
+			s/[\._]tmp//;\
+			s/\.xpm/_xpm/" > $@
 
 ###############################################################################
 # This rule can copy * to $(outdir)/*
